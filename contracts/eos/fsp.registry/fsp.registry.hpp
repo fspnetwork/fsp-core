9c9a0dd54ede54e342a0b57487d87f455cb922ae
eliquinox <eliquinox@outlook.com>
2018-12-22 17:39:50 +0000

    base contract structure added

---
+++ contracts/eos/fsp.registry/fsp.registry.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <eosiolib/asset.hpp>
+#include <eosiolib/eosio.hpp>
+#include <string>
+
+namespace fsp {
+    using std::string;
+    class registry : eosio::contract {
+        
+    }
+}
