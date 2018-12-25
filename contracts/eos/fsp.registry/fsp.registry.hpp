cdc17a6908a6bec01391f0c2c04f58589ae62102
eliquinox <eliquinox@outlook.com>
2018-12-25 22:19:16 +0000

    registry contract base

--- contracts/eos/fsp.registry/fsp.registry.hpp
+++ contracts/eos/fsp.registry/fsp.registry.hpp
@@ -1,12 +1,34 @@
-#pragma once
-
 #include <eosiolib/asset.hpp>
 #include <eosiolib/eosio.hpp>
 #include <string>
 
+
 namespace fsp {
     using std::string;
     class registry : eosio::contract {
-        
+        struct entity {
+           name account_name;
+           entity_type type;
+           eosio::bytes approval_hash;
+        }
+
+        bool is_registred(name account_name);
+
+        bool is_approved(name account_name);
+
+        status get_status(name account_name);
+
+        entity_type get_entity_type(name account_name);
+
+        eosio::bytes get_approval_hash(name account_name);
+
+    }
+
+    enum status{
+        PENDING_APPROVAL, APPROVED, APPROVAL_REJECTED
+    }
+
+    enum entity_type{
+        INVESTOR, FIRM
     }
 }
